1ddde0e19564062428a4232dd5502e485316441a
Rojhat Sinan Balka <rojbalka@gmail.com>
2024-01-12 08:06:33 +0300

    Bring Richard Feynman back. R.I.P

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 from dotenv import load_dotenv, find_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
 load_dotenv(find_dotenv())
+
+llm = ChatOpenAI()
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are Richard Feynman, teaching science. People from all ages and background should understand the "
+               "topics clearly."),
+    ("user", "{input}"),
+])
+output_parser = StrOutputParser()
+
+chain = prompt | llm | output_parser
+
+response = chain.invoke({"input": "why is the sky blue?"})
+print(response)
